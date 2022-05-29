778b4041403a0636c0c42ab3dd9c27e4de065900
kubekthecreator <50717269+kubekthecreator@users.noreply.github.com>
2022-05-29 12:07:04 +0200

    Initial commit

---
+++ WeatherRestApiApplication/src/main/java/io/kubekthecreator/WeatherRestApiApplication/WeatherRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.kubekthecreator.WeatherRestApiApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherRestApiApplication.class, args);
+	}
+
+}
---
+++ WeatherRestApiApplication/src/test/java/io/kubekthecreator/WeatherRestApiApplication/WeatherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kubekthecreator.WeatherRestApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
